1bec7cfd03efd9d4e1f1642b1dc2520c00a3f41b
AaravSureban <aaravsureban@gmail.com>
2023-11-13 14:21:31 -0500

    test

--- app/_layout.js
+++ app/_layout.js
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-
+ 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
