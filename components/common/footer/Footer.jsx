7a8dada84ebfcddd552722c50a0ddaa63f18f3bd
SahelAbraham <abrahamjsahel@gmail.com>
2023-11-14 14:37:17 -0500

    added logo asset and moved footer files

--- components/common/footer/Footer.jsx
+++ components/common/footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './footer.style'
 
 const Footer = () => {
   return (
     <View>
-      <Text>Footer</Text>
+      <Image> Footer </Image>
     </View>
   )
 }
--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,7 @@
 import tvhead from "../assets/images/tvhead.png";
+import LogoGif from "../assets/images/LogoGIF.gif"
 
 export default {
   tvhead,
+  LogoGif
 };
