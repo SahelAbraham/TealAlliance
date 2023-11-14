c5bb92ff951b4194825dbe9ac83e1538dd38749c
Ayush Singhal <aysinghal06@gmail.com>
2023-11-14 14:36:55 -0500

    Removed template materials

--- components/index.js
+++ components/index.js
@@ -6,24 +6,24 @@ import Nearbyjobs from "./home/nearby/Nearbyjobs";
 import Popularjobs from "./home/popular/Popularjobs";
 
 // job details screen
-import Company from "./jobdetails/company/Company";
-import { default as JobTabs } from "./jobdetails/tabs/Tabs";
-import { default as JobAbout } from "./jobdetails/about/About";
-import { default as JobFooter } from "./jobdetails/footer/Footer";
-import Specifics from "./jobdetails/specifics/Specifics";
+// import Company from "./jobdetails/company/Company";
+// import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+// import { default as JobAbout } from "./jobdetails/about/About";
+// import { default as JobFooter } from "./jobdetails/footer/Footer";
+// import Specifics from "./jobdetails/specifics/Specifics";
 
 // common
-import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+// import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
 
 export {
   ScreenHeaderBtn,
   Welcome,
   Nearbyjobs,
   Popularjobs,
-  Company,
-  JobTabs,
-  JobAbout,
-  JobFooter,
-  Specifics,
-  NearbyJobCard
+  // Company,
+  // JobTabs,
+  // JobAbout,
+  // JobFooter,
+  // Specifics,
+  // NearbyJobCard
 };
