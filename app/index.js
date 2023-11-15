5c5930e5960f059cbfa70b9a0a6bde384265991b
Sahel Abraham <abrahamjsahel@gmail.com>
2023-11-15 02:02:58 -0500

    some navigation things that aren't working

--- app/index.js
+++ app/index.js
@@ -1,47 +1,48 @@
-import { useState } from 'react';
+import * as React from 'react';
 import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { COLORS, icons, images, SIZES } from '../constants';
 import { ScreenHeaderBtn, Welcome } from '../components'
 import { Footer } from '../components/common/footer/Footer'
 
-const Home = () => {
 
-    const router = useRouter();
-
+function Home(){
+    
     return(
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black2 }}>
-            <Stack.Screen
-                options = {{
-                    headerStyle: {backgroundColor: COLORS.teal},
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn iconUrl={icons.menu} dimension = "125%" />
-                    ),
-                    headerRight: () => (
-                        <ScreenHeaderBtn iconUrl={images.tvhead} dimension = "225%" />
-                    ),
-                    headerTitle: "Home",
-                    headerTitleAlign: "Center"
-                }}
-                
-            />
-
-            <ScrollView showVerticalScrollIndicator = {false}>
-                <View
-                    style = {{
-                        flex: 1,
-                        padding: SIZES.medium
+            <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black2 }}>
+                <Stack.Screen
+                    options = {{
+                        headerStyle: {backgroundColor: COLORS.teal},
+                        headerShadowVisible: false,
+                        headerLeft: () => (
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension = "125%" />
+                        ),
+                        headerRight: () => (
+                            <ScreenHeaderBtn iconUrl={images.tvhead} dimension = "225%" />
+                        ),
+                        headerTitle: "Home",
+                        headerTitleAlign: "Center"
                     }}
-                    >
-                        <Welcome
-                        
-                        />
-                    </View>
-            </ScrollView>
+                    
+                />
 
-        </SafeAreaView>
+                <ScrollView showVerticalScrollIndicator = {false}>
+                    <View
+                        style = {{
+                            flex: 1,
+                            padding: SIZES.medium
+                        }}
+                        >
+                            <Welcome
+                            
+                            />
+                        </View>
+                </ScrollView>
+
+            </SafeAreaView>
     )
 }
 
