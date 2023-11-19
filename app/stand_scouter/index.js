e5c6802027967add7ede3dac5d9c34f8e307395d
Sahel Abraham <abrahamjsahel@gmail.com>
2023-11-19 17:14:18 -0500

    added forms and formcontrol to standScouter

--- app/stand_scouter/index.js
+++ app/stand_scouter/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import * as React from 'react';
 import { Stack, useRouter, Link } from 'expo-router';
 
 import { COLORS, icons, images, SIZES } from '../../constants';
@@ -15,16 +15,39 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  FlatList
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard
  } from 'react-native'
 
-const submit = () => {
-
-}
 
 export default function StandScouter(){
   const router = useRouter();
+  const submit = () => {
+    validate() ? console.log('Submitted') : console.log('Invalid');
+  }
+  const [form1Data, setForm1Data] = React.useState({});
+  const [error1, setError1] = React.useState({});
+
+  const validate = () => {
+    if (form1Data.val === undefined) {
+      setError1({ ...error1,
+        val: 'Number not inputted'
+      });
+      return false;
+    } else if (form1Data.val.length > 3) {
+      setError1({ ...error1,
+        val: 'Number Too Long'
+      });
+      return false;
+    }
+
+    return true;
+
+  };
+
    return(
+    <TouchableWithoutFeedback onPress = {Keyboard.dismiss}>
        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black2 }}>
            <Stack.Screen
                options = {{
@@ -48,21 +71,37 @@ export default function StandScouter(){
           }}>
             <Text style = {styles.BigTeal}> Hello Stand Scouter </Text>
             <NativeBaseProvider>
-            <Box alignItems={'center'} padding={4}>
-                <FormControl>
-                    <FormControl.Label> Enter Match Number </FormControl.Label>
-                <Input size = 'xl' placeholder='Match #'/>
+            <Box alignItems={'center'} padding={4} _text={{ fontsize: 'md', color: 'white'}}>
+                <FormControl isInvalid>
+                    <FormControl.Label _text = {{color: 'white'}}> Enter Match Number </FormControl.Label>
+                <Input 
+                    style = {{color: 'white'}}
+                    size = '2xl' 
+                    placeholder='Eg. 120' 
+                    keyboardType='numeric' 
+                    onChangeText={value => setForm1Data({...form1Data, val: value})}/>
+                <FormControl.ErrorMessage> Please Enter a Valid Match Number </FormControl.ErrorMessage>
                 </FormControl>
             </Box>
-            <View style = {{flex: 1, position: 'absolute', alignSelf: 'center', bottom: 20}}>
-                <Center>
-                <Box alignItems={'center'}>
-                    <Button size = 'lg' onPress={submit} colorScheme = 'teal'> Start Scouting </Button>
+            <Box alignItems={'center'} padding={4} _text={{ fontsize: 'md', color: 'white'}}>
+                <FormControl>
+                    <FormControl.Label _text = {{color: 'white'}}> Enter Team Number </FormControl.Label>
+                <Input 
+                    style = {{color: 'white'}}
+                    size = '2xl' 
+                    placeholder='Eg. 303' 
+                    keyboardType='numeric'/>
+                <FormControl.ErrorMessage> Please Enter a Valid Match Number </FormControl.ErrorMessage>
+                </FormControl>
+            </Box>
+            <View style = {{flex: 1, position: 'absolute', bottom: 20, alignSelf: 'center'}}>
+                <Box alignItems={'center'} safeAreaBottom>
+                    <Button size = 'lg' onPress={submit} colorScheme = 'teal'>Start Scouting!</Button>
                 </Box>
-                </Center>
             </View>
             </NativeBaseProvider>
           </View>
        </SafeAreaView>
+    </TouchableWithoutFeedback>
    )
 }
